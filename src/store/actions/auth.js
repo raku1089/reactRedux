92e58ee2e8a7e7413c29132f1998b267f2115061
Rakesh <rakesh@s2bnexgens-MacBook-Pro.local>
2020-05-12 04:09:40 +0530

    on refresh logout cancled by addding localstorage

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -23,6 +23,7 @@ export const authFail = (error) => {
 export const logout = () => {
   localStorage.removeItem("token");
   localStorage.removeItem("expirationDate");
+  localStorage.removeItem("userId");
   return {
     type: actionTypes.LOGOUT,
   };
@@ -58,6 +59,7 @@ export const auth = (email, password, isSignup) => {
         );
         localStorage.setItem("token", resp.data.idToken);
         localStorage.setItem("expirationDate", expirationdate);
+        localStorage.setItem("userId", resp.data.localId);
         dispatch(authSuccess(resp.data.idToken, resp.data.localId));
         dispatch(checkAuthTimeout(resp.data.expiresIn));
       })
@@ -71,5 +73,27 @@ export const setAuthRedirectPath = (path) => {
   return {
     type: actionTypes.SET_AUTH_REDIRECT,
     path: path,
+  };
+};
+
+export const authCheckState = () => {
+  return (dispatch) => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      dispatch(logout());
+    } else {
+      const expirationDate = new Date(localStorage.getItem("expirationDate"));
+      if (expirationDate < new Date()) {
+        dispatch(logout());
+      } else {
+        const userId = localStorage.getItem("userId");
+        dispatch(authSuccess(token, userId));
+        dispatch(
+          checkAuthTimeout(
+            (expirationDate.getTime() - new Date().getTime()) / 1000
+          )
+        );
+      }
+    }
   };
 };
